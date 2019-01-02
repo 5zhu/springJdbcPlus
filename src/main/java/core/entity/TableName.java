3374ef2ddddf5edd2f87e71e8f076c3ec358ce48
geguofeng <849447894@qq.com>
2019-01-02 21:01:58 +0800

    first commit

--- src/main/java/core/entity/TableName.java
+++ src/main/java/core/entity/TableName.java
@@ -5,5 +5,15 @@ package core.entity;
  * @Date: 2019/1/2
  * @Description:
  */
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface TableName {
+
+    public String value() default "";
+
+    public String pk() default "id";
 }
--- src/main/java/core/utils/SqlGenerater.java
+++ src/main/java/core/utils/SqlGenerater.java
@@ -1,9 +1,59 @@
 package core.utils;
 
+import core.entity.SuperEntity;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 /**
- * @Auther: geguofeng
- * @Date: 2019/1/1
- * @Description:
+ * Sql 生成工具类
  */
-public class SqlGenerater {
+public class SqlGenerater<T extends SuperEntity> {
+
+    private SqlGenerater(){}
+
+    private static class Holder{
+        private final static SqlGenerater instance = new SqlGenerater();
+    }
+
+    public static SqlGenerater get(){
+        return Holder.instance;
+    }
+
+    public String generateInsertSql(T t) throws Exception {
+        StringBuffer insert_sql = new StringBuffer(" insert into ");
+        StringBuffer insert_value_sql = new StringBuffer(" values (");
+        insert_sql.append(t.getTableName()).append("(");
+        List<String> fieldList = t.getAttributeNames();
+        String split = "";
+        for (String fieldName : fieldList) {
+            if (Objects.nonNull(t.getAttributeValue(fieldName))) {
+                insert_sql.append(split).append(t.getDBAttributeName(fieldName));
+                insert_value_sql.append(split).append(":").append(fieldName);
+                split = ",";
+            }
+        }
+        insert_sql.append(")");
+        insert_value_sql.append(")");
+        return insert_sql.append(insert_value_sql).toString();
+    }
+
+    public String generateDeleteSql(T t){
+        StringBuffer delete_sql = new StringBuffer(" delete from ");
+        delete_sql.append(t.getTableName()).append(" where ")
+                .append(t.getDBAttributeName(t.getPkFieldName()))
+                .append("=:id");
+        return delete_sql.toString();
+    }
+
+    public String generateSelectSql(T t) {
+        StringBuffer sql = new StringBuffer(" SELECT ");
+        String fieldInDB = t.getAttributeNames().stream()
+                .map(s -> t.getDBAttributeName(s))
+                .collect(Collectors.joining(","));
+        sql.append(fieldInDB).append(" FROM ").append(t.getTableName()).append(" ");
+        return sql.toString();
+    }
+
 }
